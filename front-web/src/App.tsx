098d1ef6963cf3adeead4c52a52086e55c4ab680
Maksuedson <maksuedson@hotmail.com>
2021-01-16 09:06:08 -0300

    Add Footer

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
+import Footer from './Footer/Index';
 import Home from './Home/Index';
 import Navbar from './Navbar/Index';
 
 function App() {
   return (
-   <>
+    <>
       <Navbar />
-      <Home/>
+      <Home />
     </>
   );
 }
