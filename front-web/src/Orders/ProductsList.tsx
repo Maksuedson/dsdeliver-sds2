1f234f674fb103431dbf7b52845847a2042be9fc
Maksuedson <maksuedson@hotmail.com>
2021-01-17 11:21:16 -0300

    Search order location

--- front-web/src/Orders/ProductsList.tsx
+++ front-web/src/Orders/ProductsList.tsx
@@ -1,8 +1,18 @@
-function ProductsList(){
+import ProductCard from './ProductCard';
+import {Product} from './types';
+
+
+type Props = {
+    products: Product[];
+}
+
+function ProductsList({products}:Props){
     return(
         <div className="orders-list-container">
-            <div className="orders-list-content">
-                Product list
+            <div className="orders-list-items">
+                {products.map(product =>(
+                    <ProductCard key={product.id} product={product}/>
+                ))}            
             </div>            
         </div>
     )
