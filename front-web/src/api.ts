2f23ff64ff0f9ecb964a4e160d6690f14801b9da
Maksuedson <maksuedson@hotmail.com>
2021-01-24 20:12:04 -0300

    Corrigido api

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,11 +1,8 @@
 import axios from "axios";
 import { OrderPayload } from "./Orders/types";
 
-
-const API_URL = 'https://maksuedson-sds2.herokuapp.com';
-const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
-
 const API_URL_BH = 'http://51.161.75.165:8081';
+const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function fetchProducts(){
     return axios(`${API_URL_BH}/products`)
