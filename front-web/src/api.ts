91fef3f029211c080b7d818f7ea33993c41d72ba
Maksuedson <maksuedson@hotmail.com>
2021-01-24 20:06:20 -0300

    Add ip bh

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -5,8 +5,10 @@ import { OrderPayload } from "./Orders/types";
 const API_URL = 'https://maksuedson-sds2.herokuapp.com';
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
+const API_URL_BH = 'http://51.161.75.165:8081';
+
 export function fetchProducts(){
-    return axios(`${API_URL}/products`)
+    return axios(`${API_URL_BH}/products`)
 }
 
 export function fetchLocalMapBox(local: string){
@@ -14,5 +16,5 @@ export function fetchLocalMapBox(local: string){
 }
 
 export function saveOrder(payload: OrderPayload){
-    return axios.post(`${API_URL}/orders`, payload)
+    return axios.post(`${API_URL_BH}/orders`, payload)
 }
